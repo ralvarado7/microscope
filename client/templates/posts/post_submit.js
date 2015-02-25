1fc5e396a5ad5809df0e55ac7a2b724d8aa7d12b
ralvarado7 <rafael.alvaradosanchez@loop.colum.edu>
2015-02-25 15:12:23 -0600

    Demonstrate the order that posts appear using a sleep.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -16,5 +16,6 @@ Template.postSubmit.events({
         alert('This link has already been posted');
       Router.go('postPage', {_id: result._id});  
     });
+       Router.go('postsList');
   }
 });
